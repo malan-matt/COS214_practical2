72b45796ea412a9b856b85c2f0ae528e117a8534
malan-matt <malan.matth@gmail.com>
2026-08-15 17:37:38 +0200

    Basic main, basic gameflow

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//INCLUDES
+#include <iostream>
+#include <string>
+using std::string;
+
+//MAIN
+int main(){
+    std::cout << "Welcome to Wayfarer!!!\n\n" << "Enter name: ";
+    string playerName;
+    std::getline(std::cin, playerName);
+
+    std::cout<<"\n\nWelcome to your journey, " << name << "\n\n"; 
+
+    bool activeGame = true;
+    while(activeGame){
+        // 1 Create world biome (Abstract Factory)
+        // 2 Generate regions (Composite and Decorator)
+        // 3 Place player at root region
+        // 4 Give player options, (list locations at current region)
+        // 5 if player wishes to move on, present the options of travel (Strategy, State)
+        // 6 Make a check to see if the player takes a loss on travel
+        // rinse and repeat or smthng
+    }
+    return 0;
+}
